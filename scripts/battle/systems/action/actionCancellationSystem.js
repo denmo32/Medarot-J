69e2d334dc4913ba1b7c2eeb9bdb7e066fbbf377
denmo32 <sf2library@gmail.com>
2025-11-28 16:55:06 +0900

    refactor: アクションキャンセル判定ロジックをデータ駆動に変更
    ActionCancellationSystemの動作トリガーをUIイベントからデータ変更イベントに変更し、ロジックの明確化と関心の分離を改善しました。
    
    変更前の課題:
    
        従来はHP_BAR_ANIMATION_COMPLETEDイベントをトリガーとしており、UIの都合でゲームロジックの実行が遅延していました。
    
        「パーツ破壊」というデータの変更と「アクションキャンセル」のロジック実行のタイミングが離れており、因果関係が追いづらい問題がありました。
    
    変更内容:
    
        監視イベントをHP_BAR_ANIMATION_COMPLETEDからPART_BROKENに変更しました。
    
        パーツ破壊イベントの発生直後に、関連するチャージ中のアクションをキャンセルするようロジックを修正しました。
    
    変更による効果:
    
        UIとコアロジックの結合度を低下させ、より堅牢な設計になりました。
    
        「データが変更されたからロジックが実行される」という自然な流れになり、コードの可読性と保守性が向上しました。

--- scripts/battle/systems/action/actionCancellationSystem.js
+++ scripts/battle/systems/action/actionCancellationSystem.js
@@ -3,51 +3,65 @@ import { GameEvents } from '../../../common/events.js';
 import { GameState, Action } from '../../components/index.js';
 import { Parts } from '../../../components/index.js';
 import { PlayerStateType, ActionCancelReason } from '../../common/constants.js';
+import { PartInfo } from '../../../common/constants.js';
 
 export class ActionCancellationSystem extends System {
     constructor(world) {
         super(world);
-        this.on(GameEvents.HP_BAR_ANIMATION_COMPLETED, this.onHpBarAnimationCompleted.bind(this));
+        // トリガーイベントをUIアニメーション完了から、より直接的なデータ変更イベントに変更
+        this.on(GameEvents.PART_BROKEN, this.onPartBroken.bind(this));
     }
 
-    onHpBarAnimationCompleted(detail) {
-        const { appliedEffects } = detail;
-        if (!appliedEffects) return;
+    /**
+     * パーツ破壊イベントをハンドルし、関連するアクションをキャンセルする。
+     * ターゲットの機能停止（頭部破壊）もここで検知する。
+     * @param {{ entityId: number, partKey: string }} detail
+     */
+    onPartBroken(detail) {
+        const { entityId: brokenEntityId, partKey: brokenPartKey } = detail;
+        const isTargetDestroyed = brokenPartKey === PartInfo.HEAD.key;
 
-        for (const effect of appliedEffects) {
-            if (!effect.isPartBroken) continue;
+        const actors = this.getEntities(GameState, Action, Parts);
+        for (const actorId of actors) {
+            const gameState = this.world.getComponent(actorId, GameState);
+            // チャージ中のアクションのみがキャンセルの対象
+            if (gameState.state !== PlayerStateType.SELECTED_CHARGING) {
+                continue;
+            }
 
-            const { targetId: brokenEntityId, partKey: brokenPartKey } = effect;
+            const action = this.world.getComponent(actorId, Action);
+            const actorParts = this.world.getComponent(actorId, Parts);
+            const selectedPart = actorParts[action.partKey];
 
-            const actors = this.getEntities(GameState, Action, Parts);
-            for (const actorId of actors) {
-                const gameState = this.world.getComponent(actorId, GameState);
-                if (gameState.state !== PlayerStateType.SELECTED_CHARGING) {
+            // 1. 自分の使用しようとしているパーツが破壊された場合
+            if (actorId === brokenEntityId && action.partKey === brokenPartKey) {
+                this.emitCancellationEvents(actorId, ActionCancelReason.PART_BROKEN);
+                continue; // このアクターの処理は完了
+            }
+            
+            // 2. ターゲットに関する破壊
+            if (selectedPart?.targetScope?.endsWith('_SINGLE') && action.targetId === brokenEntityId) {
+                // 2a. ターゲット自体が機能停止した場合（頭部破壊）
+                if (isTargetDestroyed) {
+                    this.emitCancellationEvents(actorId, ActionCancelReason.TARGET_LOST);
                     continue;
                 }
-
-                const action = this.world.getComponent(actorId, Action);
-                const actorParts = this.world.getComponent(actorId, Parts);
-                const selectedPart = actorParts[action.partKey];
-
-                if (actorId === brokenEntityId && action.partKey === brokenPartKey) {
-                    this.world.emit(GameEvents.ACTION_CANCELLED, { entityId: actorId, reason: ActionCancelReason.PART_BROKEN });
-                    this.world.emit(GameEvents.REQUEST_RESET_TO_COOLDOWN, { entityId: actorId, options: { interrupted: true } });
-                    continue;
-                }
-                
-                if (effect.isPlayerBroken && selectedPart && selectedPart.targetScope?.endsWith('_SINGLE') && action.targetId === brokenEntityId) {
-                    this.world.emit(GameEvents.ACTION_CANCELLED, { entityId: actorId, reason: ActionCancelReason.TARGET_LOST });
-                    this.world.emit(GameEvents.REQUEST_RESET_TO_COOLDOWN, { entityId: actorId, options: { interrupted: true } });
-                    continue;
-                }
-
-                if (selectedPart && selectedPart.targetScope?.endsWith('_SINGLE') && action.targetId === brokenEntityId && action.targetPartKey === brokenPartKey) {
-                    this.world.emit(GameEvents.ACTION_CANCELLED, { entityId: actorId, reason: ActionCancelReason.TARGET_LOST });
-                    this.world.emit(GameEvents.REQUEST_RESET_TO_COOLDOWN, { entityId: actorId, options: { interrupted: true } });
+                // 2b. ターゲットの特定のパーツが破壊された場合
+                if (action.targetPartKey === brokenPartKey) {
+                    this.emitCancellationEvents(actorId, ActionCancelReason.TARGET_LOST);
                     continue;
                 }
             }
         }
+    }
+
+    /**
+     * キャンセル関連のイベントを発行するヘルパーメソッド
+     * @param {number} entityId 
+     * @param {ActionCancelReason} reason 
+     */
+    emitCancellationEvents(entityId, reason) {
+        this.world.emit(GameEvents.ACTION_CANCELLED, { entityId, reason });
+        this.world.emit(GameEvents.REQUEST_RESET_TO_COOLDOWN, { entityId, options: { interrupted: true } });
     }
 }
